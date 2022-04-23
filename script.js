a65858df73e115422cabd8b11ddbc85762d25b67
oluwafemioluwatuyi <oluwafemioluwatuyi8@gmail.com>
2022-04-23 12:05:04 +0100

    gethelp

---
+++ script.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector(".btn");
+
+const result = document.querySelector(".result");
+
+
+
+btn.addEventListener("click", palindrome);
+
+
+
+
+function palindrome(){
+    const word = document.querySelector(".text").value;
+    let len = word.length;
+
+    let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+    
+    let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+
+    let flip = end.split("").reverse().join("");
+
+     if(start==flip){
+
+         result.innerHTML =  word + ` is a palindrome`
+     } else {
+        result.innerHTML =  word  + ` is not a palindrome`
+     }
+    
+
+}
+
